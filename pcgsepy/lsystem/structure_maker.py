894e0517ad27fc93b9ff8b9a3b68bdabc1f4c819
gallorob <gallotta.roberto@gmail.com>
2021-12-03 18:13:08 +0100

    Parametric L-System
    Started upgrading to parametric L-System

--- pcgsepy/lsystem/structure_maker.py
+++ pcgsepy/lsystem/structure_maker.py
@@ -20,11 +20,6 @@ class StructureMaker:
         self.position = position
         self.rotations = []
         self.position_history = []
-        # temporary, used only when extracting properties
-        # (which will need a rework)
-        self.a = ''
-        self.i = 0
-        self.axiom = ''
     
     def _apply_rotation(self,
                         arr: Vec) -> Vec:
@@ -34,44 +29,40 @@ class StructureMaker:
         return Vec.from_np(arr)
         
     def _rotate(self,
-                action_args: Any) -> int:
-        self.rotations.append(rotation_matrices[action_args])
-        return 0
+                action_args: Any) -> None:
+        self.rotations.append(rotation_matrices[action_args['action_args']])
 
     def _move(self,
-              action_args: Any) -> int:
-        action_args = action_args.value
+              action_args: Any) -> None:
+        action_args = action_args['action_args'].value
         if self.rotations:
             action_args = self._apply_rotation(arr=action_args)
         self.position = self.position.sum(action_args)
-        return 0
 
     def _push(self,
-              action_args: Any) -> int:
+              action_args: Any) -> None:
         self.position_history.append(self.position)
-        return 0
 
     def _pop(self,
-             action_args: Any) -> int:
+             action_args: Any) -> None:
         self.position = self.position_history.pop(-1)
         if self.rotations:
             self.rotations.pop(-1)
-        return 0
 
     def _place(self,
-               action_args: Any) -> int:
+               action_args: Any) -> None:
         if type(self.structure) == Structure:
-            orientation_forward, orientation_up = self.axiom[self.i + len(self.a):self.i + len(self.a) + 2]
+            # orientation_forward, orientation_up = self.axiom[self.i + len(self.a):self.i + len(self.a) + 2]
+            orientation_forward, orientation_up = action_args['parameters'][0], action_args['parameters'][1]
             orientation_forward = orientation_from_str[orientation_forward]
             orientation_up = orientation_from_str[orientation_up]
             if self.rotations:
                 orientation_forward = orientation_from_vec(self._apply_rotation(arr=orientation_forward.value))
                 orientation_up = orientation_from_vec(self._apply_rotation(arr=orientation_up.value))
-            self.structure.add_block(block=Block(block_type=action_args[0],
+            self.structure.add_block(block=Block(block_type=action_args['action_args'][0],
                                                  orientation_forward=orientation_forward,
                                                  orientation_up=orientation_up),
                                      grid_position=self.position.as_tuple())
-            return 2
         else:
             dims = self.additional_args['tiles_dimensions'][self.a].as_array()
             for r in reversed(self.rotations):
@@ -79,23 +70,27 @@ class StructureMaker:
             p = build_polyhedron(position=self.position,
                                  dims=Vec.from_np(dims))
             self.structure.add_hl_poly(p)
-            return 0
 
     def fill_structure(self,
                        structure: Structure,
                        axiom: str,
                        additional_args: Dict[str, Any] = {}) -> None:
-        self.axiom = axiom
         self.additional_args = additional_args
         self.structure = structure if structure else HLStructure()
         i = 0
         while i < len(axiom):
+            offset = 0
             for a in self.atoms_alphabet.keys():
                 if axiom.startswith(a, i):
-                    self.i = i
-                    self.a = a
+                    offset += len(a)
+                    # check for atom's parameters
+                    parameters = []
+                    if i + offset < len(axiom) and axiom[i + offset] == '(':
+                        params = axiom[i + offset:axiom.index(')', i + offset + 1) + 1]
+                        offset += len(params)
+                        parameters = params.replace('(', '').replace(')', '').split(',')
                     action, args = self.atoms_alphabet[a]['action'], self.atoms_alphabet[a]['args']
-                    i += self._calls[action](args)
-                    i += len(a)
+                    self._calls[action]({'action_args': args, 'parameters': parameters})
                     break
+            i += offset
         return self.structure
