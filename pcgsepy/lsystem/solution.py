191c445ef9141e6815ece4fc1c5c5424e246a842
gallorob <gallotta.roberto@gmail.com>
2022-02-08 15:48:17 +0100

    Resolution, age
    Add resolution increasing,  solution aging

--- pcgsepy/lsystem/solution.py
+++ pcgsepy/lsystem/solution.py
@@ -4,33 +4,31 @@ from ..structure import Structure
 
 
 class CandidateSolution:
-    def __init__(self,
-                 string: str,
-                 content: Optional[Structure] = None):
+
+    def __init__(self, string: str, content: Optional[Structure] = None):
         self.string = string
         self._content = content
 
         self.c_fitness = 0.
         self.b_descs = (0., 0.)
         self.is_feasible = False
+        self.age = 0
 
     def __str__(self) -> str:
         return self.string
-    
+
     def __repr__(self) -> str:
         return str(self)[:50] + f'; fitness: {self.c_fitness}; is_feasible: {self.is_feasible}'
 
-    def __eq__(self,
-               other: 'CandidateSolution') -> bool:
+    def __eq__(self, other: 'CandidateSolution') -> bool:
         if isinstance(other, CandidateSolution):
             return self.string == other.string
         return False
-    
+
     def __hash__(self):
         return hash(self.string)
 
-    def set_content(self,
-                    content: Structure):
+    def set_content(self, content: Structure):
         if self._content:
             raise Exception('Structure already exists for this CandidateSolution.')
         else:
