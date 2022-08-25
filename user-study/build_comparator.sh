366b01206a5b7a4363236479fddd0509533ad0c7
Kaixhin <design@kaixhin.com>
2022-08-25 14:54:19 +0900

    Fix Ubuntu PyInstaller files

--- user-study/build_comparator.sh
+++ user-study/build_comparator.sh
@@ -2,13 +2,13 @@ pyinstaller comparator_webapp_launcher.py \
 	--clean \
 	--onefile \
 	--noconfirm \
-	--name "Spaceships Comparator" \
+	--name "Spaceship Comparator" \
 	--icon assets\favicon.ico \
-	--add-data "./estimators;estimators" \
-	--add-data "./assets;assets" \
-	--add-data "./block_definitions.json;." \
-	--add-data "./common_atoms.json;." \
-	--add-data "./configs.ini;." \
-	--add-data "./hl_atoms.json;." \
-	--add-data "./hlrules;." \
-	--add-data "./llrules;."
+	--add-data "./estimators:estimators" \
+	--add-data "./assets:assets" \
+	--add-data "./block_definitions.json:." \
+	--add-data "./common_atoms.json:." \
+	--add-data "./configs.ini:." \
+	--add-data "./hl_atoms.json:." \
+	--add-data "./hlrules:." \
+	--add-data "./llrules:."
